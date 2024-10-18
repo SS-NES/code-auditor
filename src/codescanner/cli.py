3b1da37e31d400a6e8b9c444a7b9fac35c4d24e1
Serkan Girgin <girgink@gmail.com>
2024-10-18 19:00:22 +0200

    Fixed CLI debug flag

--- src/codescanner/cli.py
+++ src/codescanner/cli.py
@@ -1,18 +1,21 @@
 import codescanner
+
 import json
 from git import Repo
 from tempfile import TemporaryDirectory
 
 import click
+import logging
 
 
 @click.command(
     context_settings = {
         'show_default': True,
-    }
+    },
+    help = "Scans the code base, where PATH is the path of the code base."
 )
 @click.argument(
-    'path'
+    'path',
 )
 @click.option(
     '--debug',
@@ -20,8 +23,11 @@ import click
     default = False,
     help = "Enable debug mode."
 )
-def run(path, debug):
-    if path.startswith("http"):
+def main(path, debug):
+    if debug:
+        logging.basicConfig(level=logging.DEBUG)
+
+    if path.startswith('http'):
         with TemporaryDirectory() as temppath:
             Repo.clone_from(path, temppath)
             report = codescanner.scan(temppath)
@@ -33,4 +39,4 @@ def run(path, debug):
 
 
 if __name__ == '__main__':
-    run()
+    main()
