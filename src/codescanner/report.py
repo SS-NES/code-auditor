ca4f07f786f7208ef715248c1a9d5afe8167bcad
Serkan Girgin <girgink@gmail.com>
2024-10-21 15:28:28 +0200

    Added support for issue descriptions

--- src/codescanner/report.py
+++ src/codescanner/report.py
@@ -1,29 +1,70 @@
 """Analysis report module."""
-from pathlib import Path
+import re
 import json
 import yaml
 import jinja2
 import pypandoc
+import functools
+from pathlib import Path
 
 from .utils import get_id, OutputType
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+@functools.cache
+def get_issues() -> dict:
+    """Returns issue descriptions.
+
+    Issue descriptions are loaded from the `templates/issues.yaml` file located
+    in the package directory.
+    """
+    path = Path(__file__).parent / 'templates/issues.yaml'
+
+    logger.debug(f"Loading issues from `{path}`.")
+    with open(path, 'r', encoding='utf-8') as file:
+        items = yaml.safe_load(file)
+
+    for item in items:
+        if 'match' in item:
+            item['match'] = re.compile(item['match'])
+
+    return items
+
+
+def find_issue(msg: str) -> dict:
+    """Returns issue description matching the message.
+
+    Args:
+        msg (str): Issue message.
+
+    Returns:
+        Dictionary of the issue description.
+    """
+    for issue in get_issues():
+        if 'match' in issue:
+            if issue['math'].fullmatch(msg):
+                return issue
+        elif msg == issue['name']:
+            return issue
 
 
 class Report:
     """Analysis report class."""
 
-    def __init__(self):
-        """Initializes analysis report object."""
+    def __init__(self, path: Path):
+        """Initializes analysis report object.
+
+        Args:
+            path (Path): Path of the code base.
+        """
+        self.path = path
         self.issues = []
         self.notices = []
         self.metadata = {}
+        self.results = {}
         self.stats = {}
-
-
-    def _get_source(self, analyser, src=None):
-        if isinstance(src, Path):
-            src = src.as_posix()
-
-        return get_id(analyser) + ((':' + src) if src else '')
 
 
     def add_metadata(self, analyser, key: str, val, src=None):
@@ -37,18 +78,22 @@ class Report:
         """
         if key not in self.metadata:
             self.metadata[key] = []
-        self.metadata[key].append((val, self._get_source(analyser, src)))
+        self.metadata[key].append((val, analyser, src))
 
 
-    def add_issue(self, analyser, msg: str, src=None):
+    def add_issue(self, analyser, msg: str, path=None):
         """Adds an issue message.
 
         Args:
             analyser (Analyser): Analyser class.
             msg (str): Issue message.
-            src: Metadata attribute source (optional).
+            path: Path of the source file (optional).
         """
-        self.issues.append((msg, self._get_source(analyser, src)))
+        item = {'msg': msg, 'analyser': analyser}
+        if path:
+            item['path'] = path if isinstance(path, Path) else Path(path)
+
+        self.issues.append(item)
 
 
     def add_notice(self, analyser, msg: str, src=None):
@@ -59,7 +104,7 @@ class Report:
             msg (str): Notice message.
             src: Metadata attribute source (optional).
         """
-        self.notices.append((msg, self._get_source(analyser, src)))
+        self.notices.append((msg, analyser, src))
 
 
     def as_dict(self) -> dict:
@@ -73,7 +118,15 @@ class Report:
             else:
                 metadata[key] = items[0][0]
 
-        issues = [item[0] for item in self.issues]
+        issues = []
+        for item in self.issues:
+            issue = {
+                'msg': item['msg'],
+                'analyser': get_id(item['analyser']),
+            }
+            if 'path' in item:
+                issue['path'] = item['path'].as_posix()
+            issues.append(issue)
 
         notices = [item[0] for item in self.notices]
 
@@ -83,6 +136,16 @@ class Report:
             'notices': notices,
             'stats': self.stats if self.stats else {},
         }
+
+
+    def output_issue(self, item):
+        out = f"### {item['msg']}\n"
+
+        issue = find_issue(item['msg'])
+        if issue and 'suggestion' in issue:
+            out += issue['suggestion']
+
+        return out
 
 
     def output(self, format: OutputType=OutputType.PLAIN, path=None) -> str:
@@ -109,8 +172,10 @@ class Report:
         else:
             env = jinja2.Environment(
                 loader=jinja2.PackageLoader('codescanner'),
-                autoescape=jinja2.select_autoescape()
+                autoescape=jinja2.select_autoescape(),
+                trim_blocks=True
             )
+            env.filters['issue'] = self.output_issue
             template = env.get_template('report.md')
             out = template.render(**report)
 
--- src/codescanner/templates/report.md
+++ src/codescanner/templates/report.md
@@ -1,5 +1,35 @@
 # CodeScanner Analysis Report
 
+Code quality and conformity for software development best practices analysis
+report of {{ metadata.name if metadata.name else 'Unnamed Software'}}. The
+software is located at `{{ stats.path }}`.
+
+{% for item in notices %}
+{{ item }}
+{% endfor %}
+
+## Issues
+
+{% if issues %}
+{% for item in issues %}
+{{ item | issue }}
+{% endfor %}
+{% else %}
+No issues found.
+{% endif %}
+
+## Metadata
+
+{% if metadata %}
+{% for key, val in metadata.items() %}
+{{ key }}
+: {{ val }}
+
+{% endfor %}
+{% else %}
+No metadata found.
+{% endif %}
+
 ---
 Created by [CodeScanner](https://github.com/SS-NES/codescanner) v{{ stats.version }} on {{ stats.date.isoformat(timespec='seconds') }}.  
 {{ stats.num_dirs }} directories and {{ stats.num_files }} files are analysed, {{ stats.num_dirs_excluded }} directories were skipped.  
