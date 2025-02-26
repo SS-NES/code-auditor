6f4760b9c88355edb3577ed6a9257a3cf266b853
Serkan Girgin <girgink@gmail.com>
2025-02-26 16:12:10 +0100

    Divided community analyser

---
+++ src/codescanner/analyser/conduct.py
@@ -0,0 +1,46 @@
+"""Code of conduct analyser module."""
+from pathlib import Path
+
+from . import Analyser, AnalyserType
+from ..report import Report
+
+
+class Conduct(Analyser):
+    """Code of conduct analyser class."""
+
+    @classmethod
+    def get_type(cls) -> AnalyserType:
+        """Returns analyser type."""
+        return AnalyserType.COMMUNITY
+
+
+    @classmethod
+    def includes(cls, path: Path) -> list[str]:
+        """Returns file and directory patterns to be included in the analysis.
+
+        Args:
+            path (Path): Path of the code base.
+
+        Returns:
+            List of file and directory patterns.
+        """
+        return [
+            '/CONDUCT.*',
+            '/CODE_OF_CONDUCT.*',
+        ]
+
+
+    @classmethod
+    def analyse_content(cls, content: str, report: Report, path: Path=None) -> dict:
+        """Analyses code of conduct content.
+
+        Args:
+            content (str): Code of conduct content.
+            report (Report): Analysis report.
+            path (Path): Path of the content file (optional).
+
+        Returns:
+            Dictionary of the analysis results.
+        """
+        if path:
+            report.add_metadata(cls, 'conduct_file', path.relative_to(report.path), path)
--- src/codescanner/analyser/contributing.py
+++ src/codescanner/analyser/contributing.py
@@ -1,13 +1,12 @@
-"""Community analyser module."""
-import re
+"""Contributing guidelines analyser module."""
 from pathlib import Path
 
 from . import Analyser, AnalyserType
 from ..report import Report
 
 
-class Community(Analyser):
-    """Community analyser class."""
+class Contributing(Analyser):
+    """Contributing guidelines analyser class."""
 
     @classmethod
     def get_type(cls) -> AnalyserType:
@@ -26,29 +25,21 @@ class Community(Analyser):
             List of file and directory patterns.
         """
         return [
-            '/CONDUCT.*',
-            '/CODE_OF_CONDUCT.*',
             '/CONTRIBUTING.*',
         ]
 
 
     @classmethod
     def analyse_content(cls, content: str, report: Report, path: Path=None) -> dict:
-        """Analyses community-related content.
+        """Analyses contributing guidelines content.
 
         Args:
-            content (str): Communit-related content.
+            content (str): Contributing guidelines content.
             report (Report): Analysis report.
             path (Path): Path of the content file (optional).
 
         Returns:
             Dictionary of the analysis results.
         """
-        if not path:
-            return
-
-        if re.search(r'CONTRIBUTING', path.name, re.IGNORECASE):
+        if path:
             report.add_metadata(cls, 'contributing_file', path.relative_to(report.path), path)
-
-        elif re.search(r'CONDUCT', path.name, re.IGNORECASE):
-            report.add_metadata(cls, 'conduct_file', path.relative_to(report.path), path)
