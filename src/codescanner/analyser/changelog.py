dd7ae1b406120e0942c615a0eb5839a9b3e2e20d
Serkan Girgin <girgink@gmail.com>
2025-02-26 16:24:05 +0100

    Added initial changelog analyser

---
+++ src/codescanner/analyser/changelog.py
@@ -0,0 +1,45 @@
+"""Change log analyser module."""
+from pathlib import Path
+
+from . import Analyser, AnalyserType
+from ..report import Report
+
+
+class ChangeLog(Analyser):
+    """Change log analyser class."""
+
+    @classmethod
+    def get_type(cls) -> AnalyserType:
+        """Returns analyser type."""
+        return AnalyserType.DOCUMENTATION
+
+
+    @classmethod
+    def includes(cls, path: Path) -> list[str]:
+        """Returns file and directory patterns to be included in the analysis.
+
+        Args:
+            path (Path): Path of the code base.
+
+        Returns:
+            List of file and directory patterns.
+        """
+        return [
+            '/CHANGELOG.*',
+        ]
+
+
+    @classmethod
+    def analyse_content(cls, content: str, report: Report, path: Path=None) -> dict:
+        """Analyses change log content.
+
+        Args:
+            content (str): Change log content.
+            report (Report): Analysis report.
+            path (Path): Path of the content file (optional).
+
+        Returns:
+            Dictionary of the analysis results.
+        """
+        if path:
+            report.add_metadata(cls, 'changelog_file', path.relative_to(report.path), path)
